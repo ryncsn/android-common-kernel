514acdf4aafd7ab7614b5db49c9fdda5355fed03
Kairui Song <ryncsn@gmail.com>
2024-09-07 20:38:59 +0800

    list, data: lightweight data corruption check
    It's already paniking, error info is trivial.
    
    Change-Id: I188e77dfb33f02f1055a51b28ff8badc1bee2ff4
    Signed-off-by: Kairui Song <ryncsn@gmail.com>

--- lib/list_debug.c
+++ lib/list_debug.c
@@ -20,19 +20,10 @@
 bool __list_add_valid(struct list_head *new, struct list_head *prev,
 		      struct list_head *next)
 {
-	if (CHECK_DATA_CORRUPTION(prev == NULL,
-			"list_add corruption. prev is NULL.\n") ||
-	    CHECK_DATA_CORRUPTION(next == NULL,
-			"list_add corruption. next is NULL.\n") ||
-	    CHECK_DATA_CORRUPTION(next->prev != prev,
-			"list_add corruption. next->prev should be prev (%px), but was %px. (next=%px).\n",
-			prev, next->prev, next) ||
-	    CHECK_DATA_CORRUPTION(prev->next != next,
-			"list_add corruption. prev->next should be next (%px), but was %px. (prev=%px).\n",
-			next, prev->next, prev) ||
-	    CHECK_DATA_CORRUPTION(new == prev || new == next,
-			"list_add double add: new=%px, prev=%px, next=%px.\n",
-			new, prev, next))
+	if (CHECK_DATA_CORRUPTION(prev == NULL || next == NULL || \
+				  next->prev != prev || prev->next != next || \
+				  new == prev || new == next,
+			"list_corruption\n"))
 		return false;
 
 	return true;
@@ -46,22 +37,10 @@ bool __list_del_entry_valid(struct list_head *entry)
 	prev = entry->prev;
 	next = entry->next;
 
-	if (CHECK_DATA_CORRUPTION(next == NULL,
-			"list_del corruption, %px->next is NULL\n", entry) ||
-	    CHECK_DATA_CORRUPTION(prev == NULL,
-			"list_del corruption, %px->prev is NULL\n", entry) ||
-	    CHECK_DATA_CORRUPTION(next == LIST_POISON1,
-			"list_del corruption, %px->next is LIST_POISON1 (%px)\n",
-			entry, LIST_POISON1) ||
-	    CHECK_DATA_CORRUPTION(prev == LIST_POISON2,
-			"list_del corruption, %px->prev is LIST_POISON2 (%px)\n",
-			entry, LIST_POISON2) ||
-	    CHECK_DATA_CORRUPTION(prev->next != entry,
-			"list_del corruption. prev->next should be %px, but was %px\n",
-			entry, prev->next) ||
-	    CHECK_DATA_CORRUPTION(next->prev != entry,
-			"list_del corruption. next->prev should be %px, but was %px\n",
-			entry, next->prev))
+	if (CHECK_DATA_CORRUPTION(next == NULL || prev == NULL || \
+				  next == LIST_POISON1 || prev == LIST_POISON2 || \
+				  prev->next != entry || next->prev != entry,
+			"list_corruption\n"))
 		return false;
 
 	return true;
